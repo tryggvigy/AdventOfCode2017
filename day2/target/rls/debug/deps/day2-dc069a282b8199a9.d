3d30117c34736a42e290fc98ca7859316ca1d235
Tryggvi Gylfason <tryggvi@spotify.com>
2017-12-02 13:40:19 +0100

    day2

---
+++ day2/target/rls/debug/deps/day2-dc069a282b8199a9.d
@@ -0,0 +1,5 @@
+/Users/tryggvi/workspace/advent-of-code-2017/day2/target/rls/debug/deps/day2-dc069a282b8199a9.rmeta: /Users/tryggvi/workspace/advent-of-code-2017/day2/main.rs
+
+/Users/tryggvi/workspace/advent-of-code-2017/day2/target/rls/debug/deps/day2-dc069a282b8199a9.d: /Users/tryggvi/workspace/advent-of-code-2017/day2/main.rs
+
+/Users/tryggvi/workspace/advent-of-code-2017/day2/main.rs:
---
+++ day2/target/rls/debug/libday2.d
@@ -0,0 +1,1 @@
+/Users/tryggvi/workspace/advent-of-code-2017/day2/target/rls/debug/libday2.rmeta: /Users/tryggvi/workspace/advent-of-code-2017/day2/main.rs
